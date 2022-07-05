b573b9df818337096231dde7e822102a0f3ea9cb
Carlos Acosta <ceacosta.jobs@gmail.com>
2022-07-05 16:16:47 -0700

    refactor: uses Onboarding class on sign-up spec.

--- tests/sign-up.spec.ts
+++ tests/sign-up.spec.ts
@@ -1,12 +1,14 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from './pages/HomePage';
 import { LoginPage } from './pages/LoginPage';
+import { OnboardingPage } from './pages/OnboardingPage';
 import { SignupPage } from './pages/SignupPage';
 
 
 let homepage: HomePage;
 let loginpage: LoginPage;
 let signuppage: SignupPage;
+let onboardingpage: OnboardingPage;
 
 test.beforeEach(async ({page}) => {
     homepage = new HomePage(page);
@@ -30,8 +32,7 @@ test('should create a new CloudApp account', async ({page}) => {
     // at least 8 characters long, contain uppercase letters and a number.
     const pwd = `cloudAppTestPwd${(new Date()).getMilliseconds()}`;
     await signuppage.signUp(email, pwd);
-    await page.waitForURL('**\/onboarding\/**');
-    await expect(page.locator('data-test-id=onboarding-downloads-app-cta')).toBeVisible();
-    await expect(page.locator('data-test-id=onboarding-downloads-chrome-cta')).toBeVisible();
-
+    onboardingpage = new OnboardingPage(page);
+    await onboardingpage.waitForUrlChange();
+    await onboardingpage.verifyDownloadBtns();
 });
