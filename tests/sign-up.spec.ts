07af84e582bd3b2e097a10bbc958d92f47a93996
Carlos Acosta <ceacosta.jobs@gmail.com>
2022-07-05 15:53:44 -0700

    refactor: uses SignupPage class on sign-up spec.

--- tests/sign-up.spec.ts
+++ tests/sign-up.spec.ts
@@ -1,10 +1,12 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from './pages/HomePage';
 import { LoginPage } from './pages/LoginPage';
+import { SignupPage } from './pages/SignupPage';
 
 
 let homepage: HomePage;
 let loginpage: LoginPage;
+let signuppage: SignupPage;
 
 test.beforeEach(async ({page}) => {
     homepage = new HomePage(page);
@@ -20,18 +22,14 @@ test('should create a new CloudApp account', async ({page}) => {
     loginpage = new LoginPage(page);
     loginpage.verifyWelcomeMsg();
     await loginpage.signUpLnk.click();
-    await page.waitForURL('**\/signup'); 
-    await expect(page.locator('text=Get started')).toBeVisible();
-    const emailInput = page.locator('#email');
-    const pwdInput = page.locator('#password');
+    signuppage = new SignupPage(page);
+    await signuppage.waitForUrlChange(); 
+    await signuppage.verifyGetStartedMsg();
     // 'xatiw91450@weepm.com'
     const email = `cloudAppTestEmail${(new Date()).getMilliseconds()}@weepm.com`;
-    await emailInput.fill(email);
     // at least 8 characters long, contain uppercase letters and a number.
     const pwd = `cloudAppTestPwd${(new Date()).getMilliseconds()}`;
-    await pwdInput.fill(pwd);
-    const signUpBtn = page.locator('data-testid=regular-signup-submit');
-    signUpBtn.click();
+    await signuppage.signUp(email, pwd);
     await page.waitForURL('**\/onboarding\/**');
     await expect(page.locator('data-test-id=onboarding-downloads-app-cta')).toBeVisible();
     await expect(page.locator('data-test-id=onboarding-downloads-chrome-cta')).toBeVisible();
