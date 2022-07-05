247c8ebf817bb23f8e4b0b33c4c8acc92400ef5a
Carlos Acosta <ceacosta.jobs@gmail.com>
2022-07-05 15:01:04 -0700

    refactor: uses HomePage class on sign-up spec.

--- tests/sign-up.spec.ts
+++ tests/sign-up.spec.ts
@@ -1,16 +1,19 @@
 import { test, expect } from '@playwright/test';
+import { HomePage } from './pages/HomePage'
+
+let homepage: HomePage;
 
 test.beforeEach(async ({page}) => {
-    await page.goto('https://getcloudapp.com');
+    homepage = new HomePage(page);
+    await homepage.goto();
 });
 
 test('should load CloudApp homepage', async ({page}) => {
-    expect(await page.title(), 'CloudApp homepage title').toBe('Screen Recording Software for Mac & PC | CloudApp');
+    await homepage.verifyPageTitle();
 });
 
 test('should create a new CloudApp account', async ({page}) => {
-    const loginLnk = page.locator('a:visible', {hasText:'Log in'});
-    await loginLnk.click();
+    await homepage.loginLnk.click();
     await expect(page.locator('text=Welcome back')).toBeVisible();
     const signUpLnk = page.locator('text=Sign up for free');
     await signUpLnk.click();
