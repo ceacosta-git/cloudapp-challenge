41ddc405f89c7a88b98d892669dc2ac4fe35bf1d
Carlos Acosta <ceacosta.jobs@gmail.com>
2022-07-05 17:23:11 -0700

    refactor: uses test fixtures from BaseTest on sign-up spec.

--- tests/sign-up.spec.ts
+++ tests/sign-up.spec.ts
@@ -1,38 +1,28 @@
-import { test, expect } from '@playwright/test';
-import { HomePage } from './pages/HomePage';
-import { LoginPage } from './pages/LoginPage';
-import { OnboardingPage } from './pages/OnboardingPage';
-import { SignupPage } from './pages/SignupPage';
+import { test } from './fixtures/BaseTest';
 
-
-let homepage: HomePage;
-let loginpage: LoginPage;
-let signuppage: SignupPage;
-let onboardingpage: OnboardingPage;
-
-test.beforeEach(async ({page}) => {
-    homepage = new HomePage(page);
-    await homepage.goto();
+test.beforeEach(async ({ homePage }) => {
+    await homePage.goto();
 });
 
-test('should load CloudApp homepage', async ({page}) => {
-    await homepage.verifyPageTitle();
+test('should load CloudApp homepage', async ({ homePage }) => {
+    await homePage.verifyPageTitle();
 });
 
-test('should create a new CloudApp account', async ({page}) => {
-    await homepage.loginLnk.click();
-    loginpage = new LoginPage(page);
-    loginpage.verifyWelcomeMsg();
-    await loginpage.signUpLnk.click();
-    signuppage = new SignupPage(page);
-    await signuppage.waitForUrlChange(); 
-    await signuppage.verifyGetStartedMsg();
+test('should create a new CloudApp account', async ({ homePage, loginPage, signupPage, onboardingPage}) => {
+    await homePage.loginLnk.click();
+
+    loginPage.verifyWelcomeMsg();
+    await loginPage.signUpLnk.click();
+    
+    await signupPage.waitForUrlChange(); 
+    await signupPage.verifyGetStartedMsg();
+    
     // 'xatiw91450@weepm.com'
     const email = `cloudAppTestEmail${(new Date()).getMilliseconds()}@weepm.com`;
     // at least 8 characters long, contain uppercase letters and a number.
     const pwd = `cloudAppTestPwd${(new Date()).getMilliseconds()}`;
-    await signuppage.signUp(email, pwd);
-    onboardingpage = new OnboardingPage(page);
-    await onboardingpage.waitForUrlChange();
-    await onboardingpage.verifyDownloadBtns();
+    await signupPage.signUp(email, pwd);
+    
+    await onboardingPage.waitForUrlChange();
+    await onboardingPage.verifyDownloadBtns();
 });
